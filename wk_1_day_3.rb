1bb173588ac534d21e91051ea1f347568ebdf8e9
Graeme Brown <graemehbrown@googlemail.com>
2017-11-22 20:16:21 +0000

    Completed questions A 3 and 4 inserting into array and finding index of elemenent.

--- wk_1_day_3.rb
+++ wk_1_day_3.rb
@@ -8,14 +8,19 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 
-stops.push("Edinburgh Waverly")
-# p stops
+stops.push("Edinburgh Waverley")
+
 # 2. Add `"Glasgow Queen St"` to the start of the array
 stops.unshift("Glasgow Queen St")
-p stops
+
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+stops.insert(4, "Polmont")
+
 # 4. Work out the index position of `"Linlithgow"`
+ p stops.index("Linlithgow")
+
 # 5. Remove `"Livingston"` from the array using its name
+
 # 6. Delete `"Cumbernauld"` from the array by index
 # 7. How many stops there are in the array?
 # 8. How many ways can we return `"Falkirk High"` from the array?
